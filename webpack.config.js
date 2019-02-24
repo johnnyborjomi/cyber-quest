a55dcfb36f1c773108dd41b5e1658656f95fe943
lysenko <nikita.lysenko@spryker.com>
2019-02-24 23:47:36 +0200

    initial, webpack.conf, helpers, grid, project structure

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    // path: path.resolve(__dirname, './dist'),
+    filename: "public.js"
+    // publicPath: "dist/"
+  },
+  devServer: {
+    port: 3000
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+      }
+      // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".json"]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+    // new CopyWebpackPlugin([{ from: "src/assets/", to: "assets" }])
+  ]
+};
+
+module.exports = config;
